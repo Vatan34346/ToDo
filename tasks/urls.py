c5ae16f8fb81ef2d2a541c5e3fbb059c018816b4
Vatan34346 <paatagvichia@gmail.com>
2025-03-11 16:23:06 +0400

    added filtration by status

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,8 @@
-from django.urls  import path
-from .views import create_task, update_task, delete_task, task_all
+from django.urls import path
+from .views import create_task, update_task, delete_task, task_list, TaskListView
 
 urlpatterns = [
-    path('', task_all, name='task_list'),
+    path('', task_list, name='task_list'),
     path('new/', create_task, name='task_create'),
     path('<int:pk>/edit/', update_task, name='task_update'),
     path('<int:pk>/delete', delete_task, name='task_delete')
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView
 from .models import Task
 from .forms import TaskForm
 
 # Create your views here.
 
 
-def task_all(req):
-    tasks = Task.objects.all()
-    return render(req, 'tasks/tasks_list.html', {'tasks': tasks})
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks/tasks_list.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        status_filter = self.request.GET.get("status")
+        if status_filter:
+            queryset = queryset.filter(status=status_filter)
+        return queryset
+
+
+def task_list(request):
+    status_filter = request.GET.get('status', None)  # Получаем статус из параметра запроса
+    if status_filter:
+        tasks = Task.objects.filter(status=status_filter)
+    else:
+        tasks = Task.objects.all()
+
+    return render(request, 'tasks/tasks_list.html', {'tasks': tasks})
 
 
 def create_task(req):
@@ -42,3 +61,4 @@ def delete_task(req, pk):
         return redirect('task_list')
 
     return render(req, 'tasks/task_confirm_delete.html', {'task': task})
+
